6b0cb19dad1ace2fd9a8e1a0ffa57cf90901ecc3
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2023-03-09 00:15:46 +0600

    Add files via upload

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ for folder in *
 do
     if [ -d $folder ]
     then 
+    temper=0
+    IFS='/.'
+    j=( $folder  )
        c=0
        for file in ./$folder/*
        do
@@ -22,21 +25,49 @@ do
          then
           if(( ( ${k[2]#0} == 29 || ${k[2]#0} ==30 || ${k[2]#0} ==31)&& ${k[2]#0} ==2 ))
           then
-               echo "not valid gorib $file "  
+               echo "tempared file name :  $file " 
+               ((temper++)) 
            else 
             if(( ${k[2]#0} == 31 && ( ${k[1]#0} ==2 || ${k[1]#0} ==4|| ${k[1]#0} ==6 || ${k[1]#0} ==9 || ${k[1]#0} ==11 )))
             then
-                 echo "not valid gorib $file "
+                 echo "tempared file name :  $file"
+                 ((temper++)) 
              else 
-               echo "all good"
+             d=0
+             while IFS=$' \n' read -r -a sam
+              do
+             
+              if ((d==0))
+              then
+              d=1
+              if [ "${g[2]}" != "${sam[1]}" ]
+               then
+                 echo "tempared file name :  $file"
+                 ((temper++)) 
+              fi
+              else 
+               d=0
+               if [ "${g[3]}" != "${sam[3]}" ]
+               then
+                 echo "tempared file name :  $file"
+                 ((temper++)) 
+              fi
+                 
+              fi
+              done < "$file"
+             
+               
              fi
           fi
          else 
-            echo "not valid date $file "    
+            echo "tempared file name :  $file"   
+            ((temper++))  
          fi 
        else 
-        echo "not valid $file " 
+        echo "tempared file name :  $file" 
+        ((temper++)) 
        fi
        done
+       echo "number of temperd file in ${j[0]}: $temper"
     fi
 done       
